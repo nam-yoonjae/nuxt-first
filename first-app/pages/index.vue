e2bb6450d4ddd34b543389d2bb3645816d7a578a
cheeeeze <nyj9349@hanmail.net>
2021-04-06 22:57:57 +0900

    create vues

--- first-app/pages/index.vue
+++ first-app/pages/index.vue
@@ -1,73 +1,25 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        first-app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1>Index.vue</h1>
+      <p>1. nuxt.config.js 에서 ssr (server side rendering) 옵션을 true/false로 줄 수 있다.</p>
+      <p>2. This page is rendered on the <strong>{{ rendering }}</strong></p>
+      <p v-if="rendering === 'server'">
+        (server side 에서 첫번째로 load 또는 새로고침 되었습니다.)
+      </p>
+      <p v-if="rendering === 'client'">(client side 에서 렌더링됨.)</p>
+      <p>3. 링크는 NuxtLink to 를 이용한다</p>
+      <h2><NuxtLink to="/dynamic">동적 페이지 생성 예제</NuxtLink></h2>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData() {
+    return {
+      rendering : process.server ? 'server' : 'client'
+    }
+  }
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
